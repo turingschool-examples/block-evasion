3669144516adea9859094dec6da2f6d20de83fda
Steve Kinney <hello@stevekinney.net>
2016-03-31 07:18:25 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Block Evasion
+
+> A small game where you avoid blocks and such.
+
+To install the dependencies:
+
+```
+npm install
+```
+
+To fire up a development server:
+
+```
+npm start
+```
+
+Once the server is running, you can visit:
+
+* `http://localhost:8080/webpack-dev-server/` to run your application.
+* `http://localhost:8080/webpack-dev-server/test.html` to run your test suite in the browser.
+
+To build the static files:
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
